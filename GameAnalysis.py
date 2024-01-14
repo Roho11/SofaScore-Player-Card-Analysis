46d3f717c63926142ba2866b7d328115d92a6338
Rok <91079656+Roho11@users.noreply.github.com>
2024-01-14 08:45:02 +0100

    Add files via upload

--- GameAnalysis.py
+++ GameAnalysis.py
@@ -1,20 +1,19 @@
-#!pip install pandas
-#!pip install openpyxl
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 from datetime import datetime
 import os
 
 def div0(x,y):
-
     try:
         return round(x/y,2)
     except ZeroDivisionError:
-       return 0
+        return 0
 
 today          = datetime.today()
 today_formated = today.strftime("%Y-%m-%d")
 
-#postave_path   = f'/Users/Roho11/Desktop/pyscripts/GamesInPlay/2023-08-20'
 postave_path   = f'/Users/Roho11/Desktop/pyscripts/GamesInPlay/{today_formated}'
 analize_path   = f'/Users/Roho11/Desktop/pyscripts/CardAnalysis/{today_formated}'
 
@@ -28,35 +27,94 @@ for file in seznam_postav:
     if file not in seznam_analiz:
         
         df = pd.read_excel(postave_path+'/'+file).drop(columns='Unnamed: 0')
-        print(df)
-        #samo XI, NaN napolnimo z 0
-        result_df = df[df['Starter'] == 'XI'].fillna(0)
-        result_df = result_df.reset_index(drop=True)
-        #mPG zaenkrat se ne uporabljamo
+        #print(df)
+        #NaN napolnimo z 0
+        result_df = df.fillna(0)
+        df= result_df[df['Pos'] != 'G']
+        df = df.reset_index(drop=True)
         
-        #samo igralci, ki imajo povprecno nad 70mPG
-        #result_df = result_df[result_df['mPG'] >= 70.0]
+        #Dodamo skalkulirane metrike
         
-        #izberemo median cpg, ker je bolj odporna proti ekstremnim vrednostnim
-        cpg_median = result_df['CardPG'].median()
-        foul_median = result_df['foulPG'].median()
-        tackle_median = result_df['TacklesPG'].median()
-        #print(f'Card per game median: {cpg_median}')
-        result_df = result_df[result_df['CardPG'] >= cpg_median]
-        #median FoulPG
-        #print(f'Foul per game median: {foul_median}')
-        result_df = result_df[result_df['foulPG'] >= foul_median]
-        #median tacklesPG
-        #print(f'Tackle per game median: {tackle_median}')
-        result_df = result_df[result_df['TacklesPG'] >= tackle_median]
-        #cardPfoul
-        #result_df['CPF'] = result_df['foulPG'] / result_df['CardPG']
-        result_df['CPF'] = div0(result_df['foulPG'], result_df['CardPG'])
-        #order
-        sorted_df = result_df.sort_values(by=['CPF'], ascending=[True])
-        sorted_df.reset_index(drop=True, inplace=True)
-        out = sorted_df.head(9)
+        df['FPC'] = div0(df['foulPG'] , df['yC']+df['dRC'])
+        df['FPC'].replace(np.inf, 0, inplace=True)
+        
+        df['TPC'] = div0(df['TacklesPG'] , df['yC']+df['dRC'])
+        df['TPC'].replace(np.inf, 0, inplace=True)
+        
+        df['FTPC'] = df['TPC']+df['TPC']
+        
+        #df['FPC_div_FPG'] = div0(df['FPC'] , df['foulPG'])
+        
+        df['TouchesPC'] = div0(df['TouchesPG'] , df['yC']+df['dRC'])
+        df['TouchesPC'].replace(np.inf, 0, inplace=True)
+        
+        df['PassesPC'] = div0(df['PassesPG'] , df['yC']+df['dRC'])
+        df['PassesPC'].replace(np.inf, 0, inplace=True)
+        
+        df['PTPC'] = df['TouchesPC'] + df['PassesPC']
+        
+        df['TouchPerTacklePG'] = div0(df['TouchesPC'], df['TacklesPG'])
+        df['TouchPerTacklePG'].replace(np.inf, 0, inplace=True)
+        
+        #naredimo norme
+        #max_cpg = df['CardPG'].max()
+        #df['CardPG_norm'] = df['CardPG'].apply(lambda x: x / max_cpg)
 
+        max_ftpc = df['FTPC'].max()
+        df['FTPC_norm'] = df['FTPC'].apply(lambda x: 0 if x == 0 else 1 -  x / max_ftpc) #Manjsa vrednost je boljsa, razen ce je vrednost 0 je rezultat 0
+
+        df['tacklesWP'] = df['tacklesWP'].str.rstrip(' %').astype(float)
+        df['TacklesWP_norm'] = df['tacklesWP'].apply(lambda x: 0 if x == 0 else  1 - x / 100) #Manjsa vrednost je boljsa, razen ce je vrednost 0 je rezultat 0
+
+        df['mPG_norm'] = df['mPG'].apply(lambda x: x / 90)
+
+        df['Starter_norm'] = df['Starter'].apply(lambda x: 1 if x == 'XI' else 0)
         
+        max_ptpc = df['PTPC'].max()
+        df['PTPC_norm'] = df['PTPC'].apply(lambda x: 0 if x == 0 else 1 - div0(x, max_ptpc)) #Manjsa vrednost je boljsa, razen ce je vrednost 0 je rezultat 0
+        
+        max_tpt = df['TouchPerTacklePG'].max()
+        df['TPT_norm'] = df['TouchPerTacklePG'].apply(lambda x: 0 if x == 0 else 1 -  x / max_tpt)
+        
+        # dolocimo ponder
+        #cpg_ponder = 0.10
+        tpt_ponder = 0.30  #Touch per tackle per Game
+        ftpc_ponder = 0.20 #Foul and Tackle per Card
+        ptpc_ponder = 0.20 #Pass and Touch per Card
+        twp_ponder = 0.10  #Tackle win percentage
+        mpg_ponder = 0.10  #Minutes per game
+        pos_ponder = 0.10  #Starter or Bench (1 or 0)
+
+        ponder_sum = ftpc_ponder+twp_ponder+mpg_ponder+pos_ponder+ptpc_ponder+tpt_ponder
+        
+        #skalkuliramo koncni order
+        df['Points'] = (
+        #df['CardPG_norm'] * cpg_ponder +
+        df['FTPC_norm'] * ftpc_ponder +
+        df['TacklesWP_norm'] * twp_ponder +
+        #df['FPC'] * fpc_ponder + 
+        df['mPG_norm'] * mpg_ponder + 
+        df['Starter_norm'] * pos_ponder +
+        df['PTPC_norm'] * ptpc_ponder  +
+        df['TPT_norm'] * tpt_ponder  
+        )
+        
+        df_ordered = df.sort_values(by=['Points'], ascending=False)
+        df_ordered
+
+        df_ordered.reset_index(drop=True, inplace=True)
+        out = df_ordered.head(10)
+
         #print(sorted_df)
         out.to_excel(f'{analize_path}/{file}')
+        
+        #Vizualizacija
+        
+        plt.figure(figsize=(10, 6))
+        sns.barplot(x='Points', y='Player', hue='Pos', data=out, palette='Set1')
+        plt.xlabel('Points')
+        plt.ylabel('Player')
+        plt.title('Points by Player and Position')
+        plt.legend(title='Position')
+        plt.tight_layout()
+        plt.savefig(f'{analize_path}/{file[:-5]}.png')
